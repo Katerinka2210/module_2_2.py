8bd9e142618212bc27839fb0b344c9b4f7823610
Katerinka2210 <stavalove8739@gmail.com>
2024-10-21 15:58:19 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+first = 123
+print(first)
+second = 456
+print(second)
+third = 789
+print(third)
+if first == second and first == third and second == third:
+        print(3)
+elif first == second  or first == third or  second == third:
+        print(2)
+else:
+        print(0)
+
